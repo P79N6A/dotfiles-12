ec469c1a89e120863e51fa43a611ce6cc830db19
PBDm <pbdm915@gmail.com>
2019-04-19 18:11:15 +0800

    add hammerspoon config

---
+++ .hammerspoon/window.lua
@@ -0,0 +1,71 @@
+-- notice: 需要权限
+hypershift = {"alt", "ctrl"}
+
+hs.hotkey.bind(hypershift, "n", function()
+  if hs.window.focusedWindow() then
+    local win = hs.window.frontmostWindow()
+    local screen = win:screen()
+    local nextScreen = screen:next()
+    win:moveToScreen(nextScreen)
+  end
+end)
+
+hs.hotkey.bind(hypershift, "/", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hypershift, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hypershift, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hypershift, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hypershift, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
